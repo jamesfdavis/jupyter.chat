e5a0c4776dee4beaf75485e67d36310b1fcc1af3
James Davis <ragingsmurf@gmail.com>
2021-11-17 13:20:31 -0700

    Clean test command.

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,19 +1,16 @@
 /* eslint-disable no-undef */
 import Robot from "./../src/robot.js";
 
-// jest.mock("./../src/robot.js");
-
 describe("Entry Point -", () => {
+
   test("ChatBot able to connect.", (done) => {
     const r = new Robot();
     r.adapter.once("connected", () => {
-
       r.shutdown();
       r.adapter.close();
       done();
-
     });
     r.run();
   });
-});
 
+});
